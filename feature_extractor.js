69b5299861438ff3e31b80e73f244047fe5e9db4
abhinav-m2109 <abhinav.mittapalli05@gmail.com>
2026-08-02 16:21:30 +0530

    Add function to extract features from URLs

--- feature_extractor.js
+++ feature_extractor.js
@@ -1,1 +1,13 @@
-
+function extractURLFeatures(urlString) {
+    const url = new URL(urlString);
+    
+    return [
+        urlString.length,                        // Length of URL
+        (urlString.match(/\./g) || []).length,   // Count of dots
+        (urlString.match(/-/g) || []).length,    // Count of hyphens
+        (urlString.match(/@/g) || []).length,    // Presence of '@'
+        urlString.startsWith("https") ? 1 : 0,   // HTTPS flag
+        /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(url.hostname) ? 1 : 0, // IP in hostname
+        url.hostname.split('.').length - 1       // Subdomain count
+    ];
+}
